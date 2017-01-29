c0b3408c919cde53cd0bad535819d03f59c67818
BuildTools <unconfigured@null.spigotmc.org>
2017-01-30 00:18:43 +0400

    added methods to get the time of the call from the event

--- src/me/nikl/calendarevents/CalendarEvent.java
+++ src/me/nikl/calendarevents/CalendarEvent.java
@@ -4,6 +4,7 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 
 /**
@@ -14,14 +15,66 @@ import java.util.ArrayList;
 public class CalendarEvent extends Event implements Cancellable{
 	private static final HandlerList handlers = new HandlerList();
 	private boolean cancelled;
-	private ArrayList labels;
+	private ArrayList<String> labels;
+	private int hour, minute, year;
+	private String month;
 	
 	public CalendarEvent(ArrayList labels){
 		this.labels = labels;
+		ZonedDateTime now = ZonedDateTime.now();
+		this.minute = now.getSecond() < 20?now.getMinute(): (now = now.plusMinutes(1)).getMinute();
+		this.hour = now.getHour();
+		this.year = now.getYear();
+		this.month = now.getMonth().toString();
 	}
 	
-	public ArrayList getLabels(){
+	/**
+	 * return a list of the events labels
+	 * a label is the name of the event in the configuration file of the plugin CalendarEvents
+	 * @return labels
+	 */
+	public ArrayList<String> getLabels(){
 		return this.labels;
+	}
+	
+	/**
+	 * get the hour this event is called in
+	 * @return hour
+	 */
+	public int getHour(){
+		return this.hour;
+	}
+	
+	/**
+	 * get the month the event was called in
+	 * @return month
+	 */
+	public String getMonth(){
+		return this.month;
+	}
+	
+	/**
+	 * get the time in the format hh:mm (24h cycle)
+	 * @return hh:mm
+	 */
+	public String getTime(){
+		return (this.hour<10? "0" + this.hour: String.valueOf(this.hour)) + ":" + (this.minute<10? "0" + this.minute: String.valueOf(this.minute));
+	}
+	
+	/**
+	 * get the year the event was called in
+	 * @return year
+	 */
+	public int getYear(){
+		return this.year;
+	}
+	
+	/**
+	 * get the minute this event is called in
+	 * @return minute
+	 */
+	public int getMinute(){
+		return this.minute;
 	}
 	
 	@Override
