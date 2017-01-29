4bcdd797afd8a05504ce58df37bafd2413990ddc
BuildTools <unconfigured@null.spigotmc.org>
2017-01-30 00:24:12 +0400

    new method for the setting-up part

--- src/me/nikl/calendarevents/Timing.java
+++ src/me/nikl/calendarevents/Timing.java
@@ -2,26 +2,34 @@ package me.nikl.calendarevents;
 
 import org.bukkit.Bukkit;
 
-import java.sql.Date;
-import java.time.LocalDateTime;
+import java.time.Instant;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by niklas on 1/25/17.
  *
  * class to store the timings of a CalendarEvent
  */
-public class Timing {
-	private ArrayList<String> days;
+class Timing {
+	private ArrayList<Integer> days;
 	private ArrayList<String> dates;
 	private ArrayList<String> monthlyDates;
 	private ArrayList<String> yearlyDates;
 	private ArrayList<String> times;
 	
-	private long nextMilli;
+	long nextCall;
 	
-	public Timing(){
+	private Set<Long> relevantMillis;
+	private Set<ZonedDateTime> relevantDates;
+	private ZoneId zone;
+	private int currentMonth, currentYear, currentMonthDay, currentWeekDay;
+	
+	
+	Timing(){
 		days = new ArrayList<>();
 		monthlyDates = new ArrayList<>();
 		yearlyDates = new ArrayList<>();
@@ -29,39 +37,38 @@ public class Timing {
 		times = new ArrayList<>();
 		
 		
-		// testing
-		java.sql.Date date = new Date(System.currentTimeMillis());
-		Bukkit.getConsoleSender().sendMessage("java.sql.Date initiated with currentTimeMillis(): " + date.toString());
+		relevantDates = new HashSet<>();
+		relevantMillis = new HashSet<>();
 		
-		LocalDateTime localDateTime = LocalDateTime.now();
-		ZonedDateTime zonedDateTime = ZonedDateTime.now();
-		
-		ZonedDateTime myDate = ZonedDateTime.of(1993,8,18,0,0,0,0,ZonedDateTime.now().getZone());
-		long millis = myDate.toInstant().toEpochMilli();
-		
-		
-		Bukkit.getConsoleSender().sendMessage("LocalDateTime: " + localDateTime.toString());
-		Bukkit.getConsoleSender().sendMessage("ZonedDateTime: " + zonedDateTime.toString());
+		ZonedDateTime now = ZonedDateTime.now();
+		currentMonth = now.getMonthValue();
+		currentYear = now.getYear();
+		currentMonthDay = now.getDayOfMonth();
+		currentWeekDay = now.getDayOfWeek().getValue();
+		zone = ZoneId.systemDefault();
 	}
 	
-	public void addDay(String day){
+	void addDay(Integer day){
 		days.add(day);
 	}
 	
-	public void addTime(String time){
+	void addTime(String time){
 		times.add(time);
 	}
 	
-	public void addDate(String date){
+	void addDate(String date){
 		dates.add(date);
 	}
-	public void addMonthlyDate(String date){
+	void addMonthlyDate(String date){
 		monthlyDates.add(date);
 	}
-	public void addYearlyDate(String date){
+	void addYearlyDate(String date){
 		yearlyDates.add(date);
 	}
 	
+	/**
+	 * Set up the relevant dates
+	 */
 	public void setUp(){
 		if(Main.debug){
 			Bukkit.getConsoleSender().sendMessage("days: " + days.toString());
@@ -70,7 +77,107 @@ public class Timing {
 			Bukkit.getConsoleSender().sendMessage("yearlyDates: " + yearlyDates.toString());
 			Bukkit.getConsoleSender().sendMessage("times: " + times.toString());
 		}
+		// This loads all relevant dates including dates that are in the past
+		// This is for future plans of making it possible to run missed events
+		if(!dates.isEmpty()){
+			for(String date : dates){
+				int day, month, year;
+				String[] dateParts = date.split("\\.");
+				try{
+					day = Integer.parseInt(dateParts[0]);
+					month = Integer.parseInt(dateParts[1]);
+					year = Integer.parseInt(dateParts[2]);
+				} catch (Exception e) {
+					// cant happen since this was already checked on load
+					continue;
+				}
+				relevantDates.add(ZonedDateTime.of(year,month,day,0,0,0,0,zone));
+			}
+		}
+		if(!monthlyDates.isEmpty()){
+			for(String date : monthlyDates){
+				int day;
+				try{
+					day = Integer.parseInt(date);
+				} catch (Exception e) {
+					// cant happen since this was already checked on load
+					continue;
+				}
+				relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,day,0,0,0,0,zone));
+				relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,day,0,0,0,0,zone).plusMonths(1));
+				relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,day,0,0,0,0,zone).minusMonths(1));
+			}
+		}
+		if(!yearlyDates.isEmpty()){
+			for(String date : yearlyDates){
+				int day, month;
+				String[] dateParts = date.split("\\.");
+				try{
+					day = Integer.parseInt(dateParts[0]);
+					month = Integer.parseInt(dateParts[1]);
+				} catch (Exception e) {
+					// cant happen since this was already checked on load
+					continue;
+				}
+				relevantDates.add(ZonedDateTime.of(currentYear,month,day,0,0,0,0,zone));
+				relevantDates.add(ZonedDateTime.of(currentYear,month,day,0,0,0,0,zone).plusYears(1));
+				relevantDates.add(ZonedDateTime.of(currentYear,month,day,0,0,0,0,zone).minusYears(1));
+			}
+		}
+		if(!days.isEmpty()){
+			int diffDay;
+			for(int day : days){
+				diffDay = day - currentWeekDay;
+				if(diffDay == 0) {
+					relevantDates.add(ZonedDateTime.of(currentYear, currentMonth, currentMonthDay, 0, 0, 0, 0, zone));
+					relevantDates.add(ZonedDateTime.of(currentYear, currentMonth, currentMonthDay, 0, 0, 0, 0, zone).plusWeeks(1));
+					relevantDates.add(ZonedDateTime.of(currentYear, currentMonth, currentMonthDay, 0, 0, 0, 0, zone).minusWeeks(1));
+				} else if (diffDay > 0) {
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).plusDays(diffDay));
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).plusDays(diffDay).plusWeeks(1));
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).plusDays(diffDay).minusWeeks(1));
+				} else {
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).minusDays(diffDay));
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).minusDays(diffDay).plusWeeks(1));
+					relevantDates.add(ZonedDateTime.of(currentYear,currentMonth,currentMonthDay,0,0,0,0,zone).minusDays(diffDay).minusWeeks(1));
+				}
+			}
+		}
+		if(Main.debug)Bukkit.getConsoleSender().sendMessage("loaded " + relevantDates.size() + " days");
+		
+		for(ZonedDateTime date : relevantDates){
+			for(String timeString : times){
+				String[] timeParts = timeString.split(":");
+				int hour, min;
+				try{
+					hour = Integer.parseInt(timeParts[0]);
+					min = Integer.parseInt(timeParts[1]);
+				} catch (Exception e){
+					// can't happen, is parsed before on load
+					continue;
+				}
+				relevantMillis.add(date.plusHours(hour).plusMinutes(min).toInstant().toEpochMilli());
+			}
+		}
+		setNextMilli();
 	}
+	
+	void setNextMilli(){
+		long currentMillis = System.currentTimeMillis(), toReturn = Long.MAX_VALUE;
+		for(Long milli : relevantMillis){
+			if(currentMillis > milli) continue;
+			if(milli < toReturn){
+				toReturn = milli;
+			}
+		}
+		if(Main.debug)Bukkit.getConsoleSender().sendMessage("next date to schedule: " + ZonedDateTime.ofInstant(Instant.ofEpochMilli(toReturn), zone).toString());
+		nextCall = toReturn;
+	}
+	
+	long getNextCall(){
+		return this.nextCall;
+	}
+	
 	
 	
 }
