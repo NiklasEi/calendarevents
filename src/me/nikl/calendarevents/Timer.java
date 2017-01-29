e22df5b072bad28ae6d90ef68377786d3bac5fbf
BuildTools <unconfigured@null.spigotmc.org>
2017-01-30 00:22:03 +0400

    reCalc next millis one second before the timer runs for the first time + make sure the timer does not get of-time

--- src/me/nikl/calendarevents/Timer.java
+++ src/me/nikl/calendarevents/Timer.java
@@ -1,24 +1,60 @@
 package me.nikl.calendarevents;
 
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.time.ZonedDateTime;
 
 /**
  * Created by niklas on 1/24/17.
  *
  *
  */
-public class Timer extends BukkitRunnable{
+class Timer extends BukkitRunnable{
 	private EventsManager eventsManager;
+	private Main plugin;
 	
-	public Timer(Main plugin){
+	Timer(Main plugin){
+		this.plugin = plugin;
 		this.eventsManager = plugin.getEventsManager();
 		
-		this.runTaskTimerAsynchronously(plugin, 0, 5);
+		int diff = ZonedDateTime.now().getSecond() - 30;
+		
+		// one second before the first timer runs make sure that the next events are not shifted by one minute.
+		new BukkitRunnable(){
+			@Override
+			public void run() {
+				new BukkitRunnable(){
+					@Override
+					public void run() {
+						eventsManager.reCalcNextMillis();
+					}
+				}.runTask(plugin);
+			}
+		}.runTaskLaterAsynchronously(plugin, (diff < 0?(-diff*20 - 20):((60-diff)*20-20)));
+		
+		this.runTaskTimerAsynchronously(plugin, (diff < 0?(-diff*20):((60-diff)*20)), 20*60);
 	}
 	
 	
 	@Override
 	public void run() {
+		if(Main.debug)
+			Bukkit.getConsoleSender().sendMessage("Timer run");
+		new BukkitRunnable(){
+			@Override
+			public void run() {
+				eventsManager.callNextMinute();
+			}
+		}.runTask(plugin);
 		
+		// make sure the timer is not getting of time
+		//   tolerance: xx:20 to xx:40
+		int sec = ZonedDateTime.now().getSecond();
+		if(sec < 20 || sec > 40){
+			if(Main.debug) Bukkit.getConsoleSender().sendMessage("out of tolerance");
+			plugin.getNewTimer();
+			cancel();
+		}
 	}
 }
