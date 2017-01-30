193e440f5f384a91bd5a2977961c168e92a93d3c
BuildTools <unconfigured@null.spigotmc.org>
2017-01-30 15:06:14 +0400

    added nmsUtil

--- src/me/nikl/calendarevents/Main.java
+++ src/me/nikl/calendarevents/Main.java
@@ -1,5 +1,8 @@
 package me.nikl.calendarevents;
 
+import me.nikl.calendarevents.nms.*;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +14,7 @@ import java.util.logging.Level;
  * Main class
  */
 public class Main extends JavaPlugin{
+	private NMSUtil nms;
 	private Timer timer;
 	private EventsManager eventsManager;
 	static boolean debug = false;
@@ -22,9 +26,9 @@ public class Main extends JavaPlugin{
 	@Override
 	public void onEnable(){
 		reload();
+		setUpNMS();
 		this.eventsManager = new EventsManager(this);
 		this.timer = new Timer(this);
-		
 	}
 	
 	@Override
@@ -80,7 +84,53 @@ public class Main extends JavaPlugin{
 	}
 	
 	
+	private boolean setUpNMS() {
+		String version;
+		
+		try {
+			version = Bukkit.getServer().getClass().getPackage().getName().replace(".",  ",").split(",")[3];
+		} catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+		
+		if(debug) getLogger().info("Your server is running version " + version);
+		
+		switch (version) {
+			case "v1_10_R1":
+				nms = new NMSUtil_1_10_R1();
+				
+				break;
+			case "v1_9_R2":
+				nms = new NMSUtil_1_9_R2();
+				
+				break;
+			case "v1_9_R1":
+				nms = new NMSUtil_1_9_R1();
+				
+				break;
+			case "v1_8_R3":
+				nms = new NMSUtil_1_8_R3();
+				
+				break;
+			case "v1_8_R2":
+				nms = new NMSUtil_1_8_R2();
+				
+				break;
+			case "v1_8_R1":
+				nms = new NMSUtil_1_8_R1();
+				
+				break;
+			case "v1_11_R1":
+				nms = new NMSUtil_1_11_R1();
+				
+				break;
+		}
+		return nms != null;
+	}
 	
+	NMSUtil getNms(){
+		return this.nms;
+	}
 	
 	EventsManager getEventsManager(){
 		return this.eventsManager;
